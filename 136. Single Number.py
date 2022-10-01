b277748d8df949e1d0b44a76fb116e180b555783
BilalUsmaniy <83291180+BilalUsmaniy@users.noreply.github.com>
2022-10-01 15:44:47 -0400

    136 problem - Single Number

---
+++ 136. Single Number.py
@@ -0,0 +1,17 @@
+def singleNumber(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+
+    my_dict = {}
+    for i in nums:
+        if my_dict.get(i):
+            my_dict[i] += 1
+        else:
+            my_dict[i] = 1
+    min_value = min(my_dict.values())
+    return list(my_dict.keys())[list(my_dict.values()).index(min_value)]
+
+print(singleNumber([4, 1, 2, 1, 2]))
+
