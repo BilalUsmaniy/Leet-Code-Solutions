8c03f74fbfec8cba535934081b578d4a6b2eb5f9
BilalUsmaniy <83291180+BilalUsmaniy@users.noreply.github.com>
2022-10-15 15:39:59 -0400

    Easy

---
+++ 1025. Divisor Game.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def divisorGame(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n % 2 == 0:
+            return True
+        return False
